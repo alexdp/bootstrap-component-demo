7887505aa3e29f1f489aa54395217f0438ad1a4d
alexdp <alexdp@free.fr>
2019-06-19 23:13:42 +0200

    2 files modified

--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@ import '@webcomponents/custom-elements/src/native-shim.js';
 // Import stylesheets
 import './style.css';
 import {SampleFormComponent} from './component/sampleform/sampleform-component'; 
-
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');

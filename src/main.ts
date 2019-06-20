c13ed4281e06d82944ad24b90a155bbe65991f73
alexdp <alexdp@free.fr>
2019-06-20 14:31:43 +0200

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import {PopupComponent} from './component/popup'; 
-import 'jquery';
+import {PopupComponent} from './component/popup/popupcomponent'; 
+import $ from 'jquery';
 
 
 $(document).ready( function() {
   const appDiv: HTMLElement = document.getElementById('app');
   let mainPopup: PopupComponent = new PopupComponent();
   appDiv.appendChild(mainPopup);
+  mainPopup.show();
 });
 

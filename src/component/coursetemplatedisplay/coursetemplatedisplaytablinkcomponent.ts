fbef59f49de2b774f0c49647545ff8e9973b3603
alexdp <alexdp@free.fr>
2019-06-20 19:07:36 +0200

    1 file modified

--- src/component/coursetemplatedisplay/coursetemplatedisplaytablinkcomponent.ts
+++ src/component/coursetemplatedisplay/coursetemplatedisplaytablinkcomponent.ts
@@ -6,20 +6,12 @@ const HTMLtemplate = (courseTemplate: CourseTemplate) => require('./coursetempla
 
 export class CourseTemplateDisplayTabLinkComponent extends HTMLElement {
 
-  public courseid: number;
-  public idnumber: string;
-  public shortname: string;
-  public summary: string;
-  public thumbnail: string;
-
   private courseTemplate: CourseTemplate;
 
   constructor(courseTemplate: CourseTemplate) {
     super();
     this.courseTemplate = courseTemplate;
   }
-
-
 
   public connectedCallback(): void {
     this.innerHTML = HTMLtemplate.apply(this.courseTemplate);

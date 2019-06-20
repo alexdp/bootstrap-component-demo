03d9dafef55e694b036ee9b792c5062d1ee6a47b
alexdp <alexdp@free.fr>
2019-06-20 19:07:19 +0200

    3 files added, 3 deleted

--- src/component/coursetemplatedisplay/coursetemplatedisplaytablinkcomponent.ts
+++ src/component/coursetemplatedisplay/coursetemplatedisplaytablinkcomponent.ts
@@ -2,7 +2,9 @@ declare function require(name: string);
 import $ from 'jquery';
 import {CourseTemplate} from '../../model/coursetemplate';
 
-export class CourseTemplateDisplayComponent extends HTMLElement {
+const HTMLtemplate = (courseTemplate: CourseTemplate) => require('./coursetemplatedisplay-tablink-template.html');
+
+export class CourseTemplateDisplayTabLinkComponent extends HTMLElement {
 
   public courseid: number;
   public idnumber: string;
@@ -11,28 +13,16 @@ export class CourseTemplateDisplayComponent extends HTMLElement {
   public thumbnail: string;
 
   private courseTemplate: CourseTemplate;
-  private const Item = (courseTemplate: CourseTemplate) => `
-  <a href="${courseTemplate.iurl}" class="list-group-item">
-    <div class="image">
-      <img src="${img}" />
-    </div>
-    <p class="list-group-item-text">${title}</p>
-  </a>
-`;
 
   constructor(courseTemplate: CourseTemplate) {
     super();
     this.courseTemplate = courseTemplate;
   }
 
-$('.list-items').html([
-  { url: '/foo', img: 'foo.png', title: 'Foo item' },
-  { url: '/bar', img: 'bar.png', title: 'Bar item' },
-].map(Item).join(''));
 
 
   public connectedCallback(): void {
-    this.innerHTML = require('./coursetemplatedisplay-template.html');
+    this.innerHTML = HTMLtemplate.apply(this.courseTemplate);
   }
 
   public disconnectedCallback(): void {

d5130990a82cc92980d85e4ff08d7b367280506f
alexdp <alexdp@free.fr>
2019-06-20 15:56:35 +0200

    1 file modified

--- src/service/moodleservice.ts
+++ src/service/moodleservice.ts
@@ -2,16 +2,18 @@ import $ from 'jquery';
 
 export class MoodleService {
 
-  private token: string = "a18c33f51122e3772c9d3574b80e35cd";
-  private getTemplateEndPoints: string = "local_essecws_getcoursetemplates";
 
+
+  private token: string = "76d4aa5f0e891787a1f9cdba83e11851";
+  private baseUrl: string = "https://essec-uat.enovation.ie";
+  private templateEndPoints: string = `${this.baseUrl}/webservice/rest/server.php?wstoken=${this.token}&wsfunction=local_essecws_getcoursetemplates&moodlewsrestformat=json`;
 
   public getTemplates(): void {
     $.ajax({
-      url: 'https://api.github.com/users',
-      dataType: 'jsonp',
-      username: 'toto',
-      password: 'verysecured',
+      type: 'POST',
+      url: this.templateEndPoints,
+      data: 'parameters[categoryid]=1',
+      dataType: 'json',
       success: (data) => {
         console.log(data)
       }

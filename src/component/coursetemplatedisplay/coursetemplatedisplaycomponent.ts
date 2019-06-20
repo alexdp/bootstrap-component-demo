bcb9adadd7286ac522bfa450100f9bac69f72408
alexdp <alexdp@free.fr>
2019-06-20 18:03:43 +0200

    1 file modified

---
+++ src/component/coursetemplatedisplay/coursetemplatedisplaycomponent.ts
@@ -0,0 +1,11 @@
+import {CourseTemplate} from '../../model/coursetemplate';
+
+export class CourseTemplateDisplayComponent {
+
+  private courseTemplate: CourseTemplate;
+
+  constructor(courseTemplate: CourseTemplate) {
+    this.courseTemplate = courseTemplate;
+  }
+
+}

4e66b1a37e988ff98efde2880eeab1c25f7a2425
alexdp <alexdp@free.fr>
2019-06-20 18:17:15 +0200

    1 file modified

--- src/component/coursetemplatedisplay/coursetemplatedisplaycomponent.ts
+++ src/component/coursetemplatedisplay/coursetemplatedisplaycomponent.ts
@@ -2,14 +2,22 @@ declare function require(name: string);
 import $ from 'jquery';
 import {CourseTemplate} from '../../model/coursetemplate';
 
-export class CourseTemplateDisplayComponent {
+export class CourseTemplateDisplayComponent extends HTMLElement {
 
   private courseTemplate: CourseTemplate;
 
   constructor(courseTemplate: CourseTemplate) {
+    super();
     this.courseTemplate = courseTemplate;
+  }
+
+  public connectedCallback(): void {
+    this.innerHTML = require('./coursetemplatedisplay-template.html');
+  }
+
+  public disconnectedCallback(): void {
   }
 
 }
 
-customElements.define("custom-dashboard",CourseTemplateDisplayComponent);
+customElements.define("custom-coursetemplatedisplay",CourseTemplateDisplayComponent);

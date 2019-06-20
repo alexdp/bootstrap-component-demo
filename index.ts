17c17ad17802bc11fc29a18f5605686dd9908877
alexdp <alexdp@free.fr>
2019-06-20 14:24:20 +0200

    2 files modified

--- index.ts
+++ index.ts
@@ -1,14 +1,5 @@
 import '@webcomponents/custom-elements/src/native-shim.js';
-// Import stylesheets
 import './style.css';
-import {SampleFormComponent} from './component/sampleform/sampleform-component'; 
+import './src/main';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'jquery';
-
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-//appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
-
-let mainComponent : SampleFormComponent = new SampleFormComponent();
-appDiv.appendChild(mainComponent);
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,10 @@
-import '@webcomponents/custom-elements/src/native-shim.js';
-// Import stylesheets
-import './style.css';
 import {PopupComponent} from './component/popup'; 
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
 import 'jquery';
 
 
-$(document).ready {
+$(document).ready( function() {
   const appDiv: HTMLElement = document.getElementById('app');
   let mainPopup: PopupComponent = new PopupComponent();
-  appDiv.appendChild(mainPopup;
-
-}
+  appDiv.appendChild(mainPopup);
+});
 
